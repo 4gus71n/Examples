fd4242d557c71589b09c752782438547e4956bf0
4gus71n <agustin.tomas.larghi@gmail.com>
2020-01-13 11:03:18 -0300

    [feature/adding_room_example] Exposing the new Interactors to the feature modules through the SubComponent.

--- core/src/main/java/com/kimboo/di/modules/InteractorModule.kt
+++ core/src/main/java/com/kimboo/di/modules/InteractorModule.kt
@@ -1,11 +1,10 @@
 package com.kimboo.di.modules
 
-import com.kimboo.interactors.GetNewsInteractor
-import com.kimboo.interactors.GetNewsInteractorImpl
-import com.kimboo.interactors.GetProfileInteractor
-import com.kimboo.interactors.GetProfileInteractorImpl
+import com.kimboo.interactors.*
 import com.kimboo.repositories.NewsRepository
 import com.kimboo.repositories.ProfileRepository
+import com.kimboo.repositories.RecipesCacheRepository
+import com.kimboo.repositories.RecipesNetworkRepository
 import dagger.Module
 import dagger.Provides
 
@@ -26,6 +25,35 @@ class InteractorModule {
     ) : GetProfileInteractor {
         return GetProfileInteractorImpl(
             profileRepository = profileRepository
+        )
+    }
+
+    @Provides
+    fun provideGetRecipeByIdInteractor(
+        recipesCacheRepository: RecipesCacheRepository
+    ) : GetRecipeByIdInteractor {
+        return GetRecipeByIdInteractorImpl(
+            recipesCacheRepository = recipesCacheRepository
+        )
+    }
+
+    @Provides
+    fun provideBookmarkRecipeInteractor(
+        recipesCacheRepository: RecipesCacheRepository
+    ) : BookmarkRecipeInteractor {
+        return BookmarkRecipeInteractorImpl(
+            recipesCacheRepository = recipesCacheRepository
+        )
+    }
+
+    @Provides
+    fun provideGetAllRecipesInteractor(
+        recipesNetworkRepository: RecipesNetworkRepository,
+        recipesCacheRepository: RecipesCacheRepository
+    ) : GetAllRecipesInteractor {
+        return GetAllRecipesInteractorImpl(
+            recipesNetworkRepository= recipesNetworkRepository,
+            recipesCacheRepository = recipesCacheRepository
         )
     }
 }
