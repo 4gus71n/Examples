f952710da8c431f34e0efa7a3eb9bf36de19aafe
4gus71n <agustin.tomas.larghi@gmail.com>
2020-02-09 14:40:00 -0300

    [refactor/improving_error_handling_rxjava_example2] Improving error handling and adding a few comments in the code.

--- example1/src/main/java/com/kimboo/example1/ui/examplezip2/ExampleZip2Activity.kt
+++ example1/src/main/java/com/kimboo/example1/ui/examplezip2/ExampleZip2Activity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.snackbar.Snackbar
 import com.kimboo.example1.R
 import com.kimboo.example1.di.component.DaggerExample1ViewInjector
 import com.kimboo.example1.di.component.Example1ViewInjector
@@ -45,7 +46,6 @@ class ExampleZip2Activity : AppCompatActivity() {
             .get(ExampleZip2ViewModel::class.java)
 
         observeStateChanges()
-        observeLoadingChanges()
         observeMessageChanges()
 
         viewModelZip.fetchProfile()
@@ -71,26 +71,13 @@ class ExampleZip2Activity : AppCompatActivity() {
     private fun observeMessageChanges() {
         viewModelZip.message.observe(this, Observer {
             when (it) {
-                is ExampleZip2ViewModel.StateMessage.NoInternetConnection -> {
+                is ExampleZip2ViewModel.Message.NoInternetConnection -> {
                     onShowNoInternetConnectionSnackbar()
                 }
-                is ExampleZip2ViewModel.StateMessage.UnauthorizedError -> {
-                    onShowUnauthorizedErrorSnackbar()
-                }
-                is ExampleZip2ViewModel.StateMessage.UnknownError -> {
+                is ExampleZip2ViewModel.Message.UnknownError -> {
                     onShowUnknownErrorSnackbar()
                 }
             }
-        })
-    }
-
-    /**
-     * Observes the loading state in the ViewModel and depending on that shows the SwipeRefreshLayout's
-     * ProgressBar or not.
-     */
-    private fun observeLoadingChanges() {
-        viewModelZip.isLoading.observe(this, Observer {
-            activityExample2SwipeRefreshLayout.isRefreshing = it
         })
     }
 
@@ -106,14 +93,41 @@ class ExampleZip2Activity : AppCompatActivity() {
                 is ExampleZip2ViewModel.State.ShowProfileInformation -> {
                     onShowProfileInformation(it.profile)
                 }
+                is ExampleZip2ViewModel.State.IsLoading -> {
+                    onChangeLoadingState(it.loading)
+                }
                 is ExampleZip2ViewModel.State.HideBusiness -> {
                     onHideBusinessSection()
                 }
                 is ExampleZip2ViewModel.State.HideProfile -> {
                     onHideProfileSection()
                 }
+                is ExampleZip2ViewModel.State.UnknownError -> {
+                    onShownUnknownErrorStateView()
+                }
+                is ExampleZip2ViewModel.State.NoInternetConnection -> {
+                    onShowNoInternetConnectionStateView()
+                }
             }
         })
+    }
+
+    private fun onShownUnknownErrorStateView() {
+        activityExample2StateDisplay.show {
+            image(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            title(R.string.error_unknown_error)
+        }
+    }
+
+    private fun onShowNoInternetConnectionStateView() {
+        activityExample2StateDisplay.show {
+            image(R.drawable.ic_signal_wifi_off_black_24dp)
+            title(R.string.error_no_internet_connection)
+        }
+    }
+
+    private fun onChangeLoadingState(loading: Boolean) {
+        activityExample2SwipeRefreshLayout.isRefreshing = loading
     }
 
     private fun setupSwipeToRefreshLayout() {
@@ -123,32 +137,48 @@ class ExampleZip2Activity : AppCompatActivity() {
     }
 
     private fun onShowUnknownErrorSnackbar() {
-
-    }
-
-    private fun onShowUnauthorizedErrorSnackbar() {
-
+        Snackbar.make(
+            activityExample2Container,
+            getString(R.string.error_unknown_error),
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 
     private fun onShowNoInternetConnectionSnackbar() {
-
+        Snackbar.make(
+            activityExample2Container,
+            getString(R.string.error_no_internet_connection),
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 
     private fun onHideProfileSection() {
         activityExample2ProfileContainer.visibility = View.GONE
+        Snackbar.make(
+            activityExample2Container,
+            getString(R.string.error_fetching_profile_info),
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 
     private fun onHideBusinessSection() {
         activityExample2BusinessContainer.visibility = View.GONE
+        Snackbar.make(
+            activityExample2Container,
+            getString(R.string.error_fetching_business_skills),
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 
     private fun onShowProfileInformation(profile: ProfileInformation) {
+        activityExample2StateDisplay.hide()
         activityExample2ProfileContainer.visibility = View.VISIBLE
         activityExample2FullNameTextView.text = "${profile.firsName} ${profile.lastName}"
         activityExample2EmailTextView.text = profile.email
     }
 
     private fun onShowBusinessSkillsInformation(businessSkills: BusinessSkills) {
+        activityExample2StateDisplay.hide()
         activityExample2BusinessContainer.visibility = View.VISIBLE
         activityExample2CareerTextView.text = businessSkills.career
         activityExample2YearsTextView.text = businessSkills.yearsExperience.toString()
